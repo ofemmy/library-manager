f72accb5b5fb022d0cb7491f66383b42ccfe3d89
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-08-29 04:35:06 +0200

    fixed home page to show list of books

--- src/main/java/com/ofemmy/librarymanager/controllers/PageController.java
+++ src/main/java/com/ofemmy/librarymanager/controllers/PageController.java
@@ -1,13 +1,25 @@
 package com.ofemmy.librarymanager.controllers;
 
+import com.ofemmy.librarymanager.models.book.Book;
+import com.ofemmy.librarymanager.services.book.BookService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
-  @RequestMapping("/")
-  public String indexPage() {
-    return "index";
+  private BookService bookService;
+
+  public PageController(BookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @RequestMapping("")
+  public String indexPage(Model model) {
+    List<Book> books = this.bookService.getAllBooks();
+    model.addAttribute("books", books);
+    return "books/index";
   }
 }
